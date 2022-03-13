3ca761401842efce576172bd8653d9682faa5407
PiQuark6046 <piquark6046@protonmail.com>
2022-03-13 13:08:47 +0000

    Updated

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-setTimeout(() => {
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
 
-var Arr = ["#ListKRFilterDetectorFirefox", "#ListKRFilterDetectorEdge", "#ListKRFilterDetectorOpera", "#ListKRFilterDetectorCon", "#ListKRFilterDetectorChrome", "#ListKRFilterDetectorSafari", "#ListKRFilterDetectorWin", "#ListKRFilterDetectorMac", "#ListKRFilterDetectorAnd", "#ListKRFilterDetectoriOS"];
-for (var i = 0; i < Arr.length; i++)
-{
-    if (document.querySelector(Arr[i]).width != 1)
-    {
-        window.location = "https://github.com/List-KR/List-KR/issues/223";
-    }
-}
-window.location = "https://github.com/List-KR/List-KR";
-}, 1500);
+        var Arr = ["#ListKRFilterDetectorFirefox", "#ListKRFilterDetectorEdge", "#ListKRFilterDetectorOpera", "#ListKRFilterDetectorCon", "#ListKRFilterDetectorChrome", "#ListKRFilterDetectorSafari", "#ListKRFilterDetectorWin", "#ListKRFilterDetectorMac", "#ListKRFilterDetectorAnd", "#ListKRFilterDetectoriOS"];
+        for (var i = 0; i < Arr.length; i++)
+        {
+            if (document.querySelector(Arr[i]).width != 1)
+            {
+                window.location = "https://github.com/List-KR/List-KR/issues/223";
+            }
+        }
+        window.location = "https://github.com/List-KR/List-KR";
+        }, 3500);
+});
