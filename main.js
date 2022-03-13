901e5b044bf81abb0947ced7717625031569049e
PiQuark6046 <piquark6046@protonmail.com>
2022-03-13 12:57:06 +0000

    Update blank

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+setTimeout(() => {
+
+var Arr = ["#ListKRFilterDetectorFirefox", "#ListKRFilterDetectorEdge", "#ListKRFilterDetectorOpera", "#ListKRFilterDetectorCon", "#ListKRFilterDetectorChrome", "#ListKRFilterDetectorSafari", "#ListKRFilterDetectorWin", "#ListKRFilterDetectorMac", "#ListKRFilterDetectorAnd", "#ListKRFilterDetectoriOS"];
+for (var i = 0; i < Arr.length; i++)
+{
+    if (document.querySelector(Arr[i]).style.display == "none")
+    {
+        window.location = "https://github.com/List-KR/List-KR/issues/223";
+    }
+}
 window.location = "https://github.com/List-KR/List-KR";
+}, 1500);
