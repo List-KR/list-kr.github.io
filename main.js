e45ffa70ce238edc798d2c1f67dc9954684e0b22
PiQuark6046 <piquark6046@protonmail.com>
2022-03-13 13:04:01 +0000

    Update blank

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ setTimeout(() => {
 var Arr = ["#ListKRFilterDetectorFirefox", "#ListKRFilterDetectorEdge", "#ListKRFilterDetectorOpera", "#ListKRFilterDetectorCon", "#ListKRFilterDetectorChrome", "#ListKRFilterDetectorSafari", "#ListKRFilterDetectorWin", "#ListKRFilterDetectorMac", "#ListKRFilterDetectorAnd", "#ListKRFilterDetectoriOS"];
 for (var i = 0; i < Arr.length; i++)
 {
-    if (document.querySelector(Arr[i]).style.display == "none")
+    if (document.querySelector(Arr[i]).width != 1)
     {
         window.location = "https://github.com/List-KR/List-KR/issues/223";
     }
