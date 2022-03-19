877e99e7c743796166fe841aded838593395ebde
PiQuark6046 <piquark6046@protonmail.com>
2022-03-19 10:52:40 +0000

    Update js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
+        var Arr = ["#ListKRFilterDetectorFirefox", "#ListKRFilterDetectorEdge", "#ListKRFilterDetectorOpera", "#ListKRFilterDetectorCon", "#ListKRFilterDetectorChrome", "#ListKRFilterDetectorSafari", "#ListKRFilterDetectorWin", "#ListKRFilterDetectorMac", "#ListKRFilterDetectorAnd", "#ListKRFilterDetectoriOS"];
+        for (var i = 0; i < Arr.length; i++)
+        {
+            if (document.querySelector(Arr[i]).width != 1)
+            {
+                window.location = "https://github.com/List-KR/List-KR#support";
+            }
+        }
         window.location = "https://github.com/List-KR/List-KR";
     }, 3500);
 });
