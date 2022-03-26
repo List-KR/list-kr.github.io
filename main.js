db154d0203cb27f55d97833701e07237ec802ee6
green1052 <34261355+green1052@users.noreply.github.com>
2022-03-26 21:56:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         var Arr = ["#ListKRFilterDetectorFirefox", "#ListKRFilterDetectorEdge", "#ListKRFilterDetectorOpera", "#ListKRFilterDetectorCon", "#ListKRFilterDetectorChrome", "#ListKRFilterDetectorSafari", "#ListKRFilterDetectorWin", "#ListKRFilterDetectorMac", "#ListKRFilterDetectorAnd", "#ListKRFilterDetectoriOS"];
-        for (var i = 0; i < Arr.length; i++)
-        {
-            if (document.querySelector(Arr[i]).width != 1)
-            {
-                window.location = "https://github.com/List-KR/List-KR#support";
-            }
+        for (var key of Arr) {
+            if (document.querySelector(ley).width !== 1)
+                 window.location = "https://github.com/List-KR/List-KR#support";
         }
         window.location = "https://github.com/List-KR/List-KR";
     }, 3500);
