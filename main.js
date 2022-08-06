265886b9eba84e4590d41238bfb3f004261d5a0f
PiQuark6046 <piquark6046@proton.me>
2022-08-06 17:29:39 +0000

    Update Index

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        var Arr = ["#ListKRFilterDetectorFirefox", "#ListKRFilterDetectorEdge", "#ListKRFilterDetectorOpera", "#ListKRFilterDetectorCon", "#ListKRFilterDetectorChrome", "#ListKRFilterDetectorSafari", "#ListKRFilterDetectorWin", "#ListKRFilterDetectorMac", "#ListKRFilterDetectorAnd", "#ListKRFilterDetectoriOS"];
-        for (var key of Arr) {
-            if (document.querySelector(key).width !== 1)
-                 window.location = "https://github.com/List-KR/List-KR#support";
-        }
-        window.location = "https://github.com/List-KR/List-KR";
-    }, 3500);
+        window.location = "https://github.com/List-KR";
+    }, 1000);
 });
