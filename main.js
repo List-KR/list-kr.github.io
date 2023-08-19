33d2d0cde8f82a52b78ad7249bbf17d1080ab786
PiQuark6046 <piquark6046@proton.me>
2023-08-19 16:27:45 +0000

    change to List-KR wiki

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        window.location = "https://github.com/List-KR/List-KR#list-kr";
+        window.location = "https://github.com/List-KR/List-KR/wiki";
     }, 1000);
 });
