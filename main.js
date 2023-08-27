02b11256ee8ab6566c776ce36d3635a3a3b5efaa
PiQuark6046 <piquark6046@proton.me>
2023-08-27 17:16:10 +0000

    update url

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        window.location = "https://github.com/List-KR/List-KR/wiki";
+        window.location = "https://list-kr-community.pages.dev";
     }, 1000);
 });
